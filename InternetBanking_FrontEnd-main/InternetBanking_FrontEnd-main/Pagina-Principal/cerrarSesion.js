37de850a9d003681a702df1d75c8223e490953ac
Jeremyrtz1120 <163479511+Jeremyrtz1120@users.noreply.github.com>
2025-02-14 17:47:52 -0400

    paneles/modal

---
+++ InternetBanking_FrontEnd-main/InternetBanking_FrontEnd-main/Pagina-Principal/cerrarSesion.js
@@ -0,0 +1,4 @@
+document.getElementById('cerrarSesion').addEventListener('click', function() {
+    alert('Simulando el cierre de sesión...');
+    // Aquí podrías agregar código para cerrar la sesión del usuario (si tuvieras un backend)
+});
