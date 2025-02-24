cd1293ddbca54e3c109b66eb07233017bebf8974
Jeremyrtz1120 <163479511+Jeremyrtz1120@users.noreply.github.com>
2025-02-24 11:09:38 -0400

    Actualizacion

---
+++ InternetBanking_FrontEnd-main/InternetBanking_FrontEnd-main/Pagina-Principal/verTarjetas.js
@@ -0,0 +1,4 @@
+document.getElementById('verTarjetas').addEventListener('click', function() {
+    alert('Simulando la consulta de tarjetas...');
+    // Aquí podrías agregar código para mostrar información real de las tarjetas (si tuvieras un backend)
+});
