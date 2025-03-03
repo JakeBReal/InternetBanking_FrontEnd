38f15ad611b8a422cd17d3fed83c9152c3dc8e28
JakeBReal <jakebencosme-8033@hotmail.com>
2025-03-02 22:14:49 -0600

    organizado

--- Pagina-Principal/cerrarSesion.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('cerrarSesion').addEventListener('click', function() {
-    alert('Simulando el cierre de sesión...');
-    // Aquí podrías agregar código para cerrar la sesión del usuario (si tuvieras un backend)
-});
